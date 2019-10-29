a8405d61bf17f302899207dbb8d5e02f07d9b445
Irtaza Nadeem <irtaza.nadeem542@gmail.com>
2019-10-29 04:47:28 -0700

    asd

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/webhook', (req, res) => {
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+    let challenge = req.query['hub.challenge'];     
 
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
